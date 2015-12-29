f00cbf2f87b4c2cd85735093430513736174ca21
jr.rombaldo <jr.rombaldo@gmail.com>
2015-12-29 18:26:08 +0000

    copyright

--- src/main/java/jrrombaldo/pset/gui/WebSearchMainForm.java
+++ src/main/java/jrrombaldo/pset/gui/WebSearchMainForm.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2015 jrrombaldo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Jr.Rombaldo
+ *******************************************************************************/
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
--- src/main/java/jrrombaldo/pset/searchengine/BingSearch.java
+++ src/main/java/jrrombaldo/pset/searchengine/BingSearch.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2015 jrrombaldo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Jr.Rombaldo
+ *******************************************************************************/
 package jrrombaldo.pset.searchengine;
 
 import java.io.IOException;
